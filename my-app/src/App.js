55567710fb97228994eec1d7b7f27471a5af9519
suleymanak006 <suleymanak0006@gmail.com>
2021-04-02 02:29:28 +0200

    profilm sayfam

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Profil from "./components/profil";
+
+import './App.css';
+
+function App () {
+  
+    return (
+   
+      <div className="App">
+      <h1 style = {{color:"red" }} align="center" >Profil Sayfam</h1>
+      <Profil/>
+      </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/profil.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+ function ProfilList() {
+    
+        return (
+            <div >
+                <div >
+                <img src="./src/kartal.jpg" alt="Profil resmim" width="200" height="200"/>
+                </div>
+                <p> <b>Ad Soyad</b> : Suleyman Ak</p>
+                <p> <b>Dogum tarihi</b> : 05/01/1985</p>
+                <p> <b>Cinsiyet</b> :Erkek</p>
+                <h4> Calisma Gecmisiniz:</h4>
+                <p>*2005-2009 yillari arasinda ozel sektor ogretmenlik</p>
+                <p>*2009-2010 yillari arasinda memuriyet</p>
+                <p>*2010-2016 yillari arasinda TRT muhabir</p>
+                <p>*2016-2020 yillari arasinda Serbest Meslek</p>
+                <h4> Egitim Gecmisiniz:</h4>
+                <p>*1988-1996 Ankara Ilkogretim </p>
+                <p>*1996-2000 Ankara Ortaogretim </p>
+                <p>*2001-2005 Lisans Fen Bilimleri Ogretmenlik </p>
+                <p>*2005-2008 Yuksek Lisans Fen Bilimleri </p>
+                <h4> Hobiler</h4>
+                <div><img src="./futbol.jpg" alt="futbol" width="300" height="200"/></div>
+                <p><img src="./sinema.jpg" alt="sinema" width="300" height="200"/></p>
+                <p><img src="./bisiklet.jpg" alt="bisiklet" width="300" height="200"/></p>
+            </div>
+        );
+};
+
+
+export default ProfilList;
